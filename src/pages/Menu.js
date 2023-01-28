0777655d1fa1c361ee6fe6519b756f2cc99f2816
Mark Maksi <mmaksi.dev@gmail.com>
2023-01-28 14:27:12 +0100

    Updating the images

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -8,7 +8,7 @@ const Menu = () => {
       <h1 className="menu-header">Week's specials</h1>
       <div className="menu">
         <Card
-          image="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4"
+          image="https://thecafesucrefarine.com/wp-content/uploads/The-Best-Greek-Salad.jpg"
           title="Greek Salad"
           price="$40"
           description="The farmous greek salad of crispy lettuce, peppers, 
@@ -16,7 +16,7 @@ const Menu = () => {
           garnished with crunchy garlic and rosemary croutons."
         />
         <Card
-          image="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4"
+          image="https://www.cucchiaio.it/content/cucchiaio/it/ricette/2009/11/ricetta-bruschetta-pomodoro/_jcr_content/header-par/image_single.img.jpg/1596625322419.jpg"
           title="Bruchetta"
           price="$40"
           description="The farmous greek salad of crispy lettuce, peppers,
@@ -24,7 +24,7 @@ const Menu = () => {
            with crunchy garlic and rosemary croutons."
         />
         <Card
-          image="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4"
+          image="https://img.taste.com.au/OKe3yYqX/taste/2016/11/little-lemon-dessert-cakes-84569-1.jpeg"
           title="Lemon Dessert"
           price="$40"
           description="This comes straight from grandma's recipe book, every last 
